cfc0c3387a07e71498c1c0fd3c7081501803d1c6
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2021-11-30 12:41:25 +0100

    :construction: json to md

--- services/src/Conversió JSON a Markdown.py
+++ services/src/Conversió JSON a Markdown.py
@@ -1,5 +1,10 @@
 import json
 
+#Converir JSON data a Diccionarios python
 data = json.loads({})     #Para decodificar una cadena en el formato JSON (es decir, convertirla a un objeto de Python).
 json.dumps()
 
+# Opening JSON file
+with open ('data.json') as json_file:
+    data = json.load(# json file in data base)
+print(data[''])
