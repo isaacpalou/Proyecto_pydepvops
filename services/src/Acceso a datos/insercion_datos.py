52be85b694e18f7133c6a1f0a8e2832b8ba62a3f
isaacpalou <_palougijon@cifpfbmoll.eu>
2021-12-02 18:10:22 +0100

    fix: bug resuelto

--- services/src/Acceso a datos/insercion_datos.py
+++ services/src/Acceso a datos/insercion_datos.py
@@ -4,17 +4,17 @@ import json
 from conexion_bd import conexion_bd
 
 
-def insercion_datos():
-    collection = conexion_bd()
-    with open("services/src/Acceso a datos/menus.json", "r") as my_json:
-        datos = json.load(my_json)
-    try:
-        collection.insert_many(datos)
-        print("DATOS METIDOS CON ÉXITO")
-    except pymongo.errors.ServerSelectionTimeoutError as error:
-        print("Error al conectar al servidor") % error
-    except pymongo.errors.CollectionInvalid as error:
-        print("No se pudo conectar a MongoCompass") % error
+# def insercion_datos():
+#     collection = conexion_bd()
+#     with open("services/src/Acceso a datos/menus.json", "r") as my_json:
+#         datos = json.load(my_json)
+#     try:
+#         collection.insert_many(datos)
+#         print("DATOS METIDOS CON ÉXITO")
+#     except pymongo.errors.ServerSelectionTimeoutError as error:
+#         print("Error al conectar al servidor") % error
+#     except pymongo.errors.CollectionInvalid as error:
+#         print("No se pudo conectar a MongoCompass") % error
 
 # def borrar_coleccion():
 #     collection = conexion_bd()
