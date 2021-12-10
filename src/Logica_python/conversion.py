f78667cc3d1add214ce2eb84dd87ae49b992fea5
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2021-12-10 11:07:02 +0100

    feat:Planteamiento conversion a md prueba

--- src/Logica_python/conversion.py
+++ src/Logica_python/conversion.py
@@ -1,5 +1,13 @@
 from pprint import pprint
+from pymongo import MongoClient
+from Proyecto_pydepvops.src.Acceso_a_datos.conexion_bd import conexion_bd
+from Proyecto_pydepvops.src.Acceso_a_datos.extraccion_datos import extraccion_datos
 
 
-def conversion_md(lista):
-    pprint(lista)
+def titulos():
+    if "#"
+
+def subtitulos():
+
+def texto_comun():
+
